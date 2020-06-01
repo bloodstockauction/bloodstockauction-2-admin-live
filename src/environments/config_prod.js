f70f832c7ae48f64eb5cca49aca904025a005a23
jihoon <hsboykjh@gmail.com>
2020-06-01 11:13:35 +1000

    added dev/prod configs

---
+++ src/environments/config_prod.js
@@ -0,0 +1,10 @@
+const config = {
+  AUTH_ID: "bloodstockauction_admin_id_token",
+  SOCKET_END_POINT: "https://bondi.bloodstockauction.com",
+  BACKEND_END_POINT_PUBLIC:
+    "https://bsa2-admin-backend.bloodstockauction.com/public",
+  BACKEND_END_POINT_COMMIN:
+    "https://bsa2-admin-backend.bloodstockauction.com/common",
+};
+
+export default config;
