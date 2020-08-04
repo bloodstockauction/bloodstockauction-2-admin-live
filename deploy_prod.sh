dcf66a25319112e4550f1febf3c71b9c51144391
jihoon <hsboykjh@gmail.com>
2020-08-04 12:08:36 +1000

    changed US deployment settings

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -6,13 +6,13 @@ npm run build
 cp ./src/environments/config_dev.js ./src/environments/config.js 
 
 ##set AWS profile
-export AWS_DEFAULT_PROFILE=bloodstockauction2
+export AWS_DEFAULT_PROFILE=bloodstockauction2-us
 
 #delete old version files
-aws s3 rm s3://bloodstockauction2-admin-live/ --recursive
+aws s3 rm s3://bloodstockauction2-us-admin-live/ --recursive
 
 #upload web application files
-aws s3 sync ./public/ s3://bloodstockauction2-admin-live/
+aws s3 sync ./public/ s3://bloodstockauction2-us-admin-live/
 
 #invalidation for new Web resources
-aws cloudfront create-invalidation --distribution-id E2VPVRY6G8PIEO --paths "/*"
+# aws cloudfront create-invalidation --distribution-id E2VPVRY6G8PIEO --paths "/*"
