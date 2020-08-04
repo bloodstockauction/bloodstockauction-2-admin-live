55f85d3195899858eb2f9abebb8fad5daafec130
jihoon <hsboykjh@gmail.com>
2020-08-04 12:06:37 +1000

    added deploy scripts

---
+++ deploy_dev.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp ./src/environments/config_dev.js ./src/environments/config.js 
+npm run build
---
+++ deploy_prod.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cp ./src/environments/config_prod.js ./src/environments/config.js 
+npm run build
+
+cp ./src/environments/config_dev.js ./src/environments/config.js 
+
+##set AWS profile
+export AWS_DEFAULT_PROFILE=bloodstockauction2
+
+#delete old version files
+aws s3 rm s3://bloodstockauction2-admin-live/ --recursive
+
+#upload web application files
+aws s3 sync ./public/ s3://bloodstockauction2-admin-live/
+
+#invalidation for new Web resources
+aws cloudfront create-invalidation --distribution-id E2VPVRY6G8PIEO --paths "/*"
